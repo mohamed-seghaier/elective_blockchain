b7d6d1e1b2d18132c7be0585f1c4299373cc44ac
SEGHAIER Mohamed-Ali <91876112+mohamed-seghaier@users.noreply.github.com>
2023-03-09 12:03:15 +0100

    Update Marketplace.sol

--- Marketplace.sol
+++ Marketplace.sol
@@ -1,1 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
 
+contract Marketplace {
+    
+    enum ShippingStatus { Pending, Shipped, Delivered }
+    
+    ShippingStatus public status;
+    
+    event MissionComplete(address indexed buyer, uint indexed orderId);
+    
+    constructor() {
+        status = ShippingStatus.Pending;
+    }
+    
+    function Shipped() public onlyOwner {
+        status = ShippingStatus.Shipped;
+    }
+    
+    function Delivered() public onlyOwner {
+        status = ShippingStatus.Delivered;
+        emit MissionComplete(msg.sender, block.number);
+    }
+    
+    function getStatus() public view onlyOwner returns (ShippingStatus) {
+        return status;
+    }
+    
+    function Status() public payable returns (ShippingStatus) {
+        require(msg.value > 0, "You need to pay to get the status.");
+        return status;
+    }
+    
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can call this function.");
+        _;
+    }
+    
+    address public owner = msg.sender;
+}
